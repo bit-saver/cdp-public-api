daa3ae75f5484695b19b0e3632f9c032940a576b
Terri Regan <contact@terriregan.com>
2018-04-27 06:17:34 -0400

    More testing

--- src/middleware/cleanTempFiles.js
+++ src/middleware/cleanTempFiles.js
@@ -2,6 +2,7 @@ import tempFiles from '../services/tempfiles';
 
 const cleanTempFilesCtrl = ( err, req, res, next ) => {
   if ( !req.indexed ) console.warn( 'REQUEST IS NOT INDEXED' );
+  console.log( `---deleteTempFiles--- ${req.requestId}` );
   tempFiles.deleteTempFiles( req.requestId );
   if ( err ) return next( err );
   next();
