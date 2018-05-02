aba89e9575a2471fb03ea2746fb45f3e73930fd3
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-05-02 07:47:09 -0700

    HOT FIX: Removed extra console.log.

--- src/middleware/cleanTempFiles.js
+++ src/middleware/cleanTempFiles.js
@@ -1,7 +1,6 @@
 import tempFiles from '../services/tempfiles';
 
 export const cleanTempFilesErrorCtrl = ( err, req, res, next ) => {
-  console.log( 'cleanTempFilesCtrl' );
   if ( !req.indexed ) console.warn( 'REQUEST IS NOT INDEXED' );
   tempFiles.deleteTempFiles( req.requestId );
   if ( err ) return next( err );
@@ -9,7 +8,6 @@ export const cleanTempFilesErrorCtrl = ( err, req, res, next ) => {
 };
 
 export const cleanTempFilesCtrl = ( req, res, next ) => {
-  console.log( 'cleanTempFilesCtrl' );
   if ( !req.indexed ) console.warn( 'REQUEST IS NOT INDEXED' );
   tempFiles.deleteTempFiles( req.requestId );
   next();
