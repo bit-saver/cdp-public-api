1328217b77093973c95f194965a012a80db4a92a
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-05-02 08:26:25 -0700

    HOT FIX: next wasn't being called when async transfers were empty.

--- src/middleware/transfer.js
+++ src/middleware/transfer.js
@@ -259,7 +259,7 @@ export const transferCtrl = Model => async ( req, res, next ) => {
  */
 export const asyncTransferCtrl = Model => async ( req, res, next ) => {
   console.log( 'ASYNC TRANSFER CONTROLLER INIT', req.requestId );
-  if ( !req.asyncTransfers || req.asyncTransfers.length < 1 ) return null;
+  if ( !req.asyncTransfers || req.asyncTransfers.length < 1 ) return next();
 
   let updated = false;
   const model = new Model();
