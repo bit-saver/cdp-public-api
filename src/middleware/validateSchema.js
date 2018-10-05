5043b9d163b07393dd9c582aa0078d2401d5b753
Tyler Hebenstreit <tyler.hebenstreit@agencychief.com>
2018-10-05 10:34:16 -0700

    HOT FIX: Adding log message via the validate method that logs a message if thumbnail data is missing.

--- src/middleware/validateSchema.js
+++ src/middleware/validateSchema.js
@@ -5,5 +5,8 @@ export const validate = ( Model, useDefaults = true ) => async ( req, res, next 
   if ( !result.valid ) {
     return next( new Error( compileValidationErrors( result.errors ) ) );
   }
+  if ( !req.body.thumbnail || !req.body.thumbnail.sizes ) {
+    console.log( 'Missing thumbnail', '\r\n', JSON.stringify( req.body, null, 2 ) );
+  }
   next();
 };
