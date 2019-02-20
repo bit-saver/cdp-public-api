33cf11ba1b9948572c87f9e8dc2691e44c62db4b
Tyler Hebenstreit <tyler.hebenstreit@agencychief.com>
2019-02-20 07:41:25 -0800

    CDP-1171: Investigate Communication Between Servers
    Added socket.io and a small test.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ require( 'newrelic' );
 
 import http from 'http';
 import app from './server';
+import socketio from 'socket.io';
 
 // Used for module hot reloading, will maintain state
 const server = http.createServer( app );
 let currentApp = app;
 const PORT = process.env.PORT || 8080;
+const io = socketio( server );
 
 server.listen( PORT, () => {
   console.log( `CDP service listening on port: ${PORT}` );
+} );
+
+io.on( 'connection', ( socket ) => {
+  console.log( 'Client connected' );
+  socket.on( 'video', ( data ) => {
+    console.log( data );
+  } );
 } );
 
 if ( module.hot ) {
