938d4b0cc6fa112e7d77f07538e2955d523dc118
Terri Regan <contact@terriregan.com>
2019-07-11 14:18:56 -0400

    Removed hot load; updated some dependencies

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import {} from 'dotenv/config';
-import apm from 'elastic-apm-node/start';
+// import apm from 'elastic-apm-node/start';
 
-require( 'newrelic' );
+// require( 'newrelic' );
 
 import http from 'http';
 import app from './server';
 
 // Used for module hot reloading, will maintain state
 const server = http.createServer( app );
-let currentApp = app;
+// const currentApp = app;
 const PORT = process.env.PORT || 8080;
 
 server.listen( PORT, () => {
   console.log( `CDP service listening on port: ${PORT}` );
 } );
-
-if ( module.hot ) {
-  module.hot.accept( ['./server'], () => {
-    server.removeListener( 'request', currentApp );
-    server.on( 'request', app );
-    currentApp = app;
-  } );
-}
